65fcf6f9d6fc77d4d4f85c5362cff0f9c2e4415a
Aditya Bisht <adpycloud@gmail.com>
2026-01-06 14:25:49 +0530

    Day 1

---
+++ Java/src/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("I like chess");
+        System.out.println("It fun to play ");
+        System.out.println("Everybody should play it");
+    }
+}
---
+++ Java/src/variable.java
@@ -0,0 +1,36 @@
+public class variable {
+    public static void main(String[] args) {
+
+        //int , boolean , double , char
+
+        int number = 10;
+        double price = 1.99;
+        char grade = 'A';
+        boolean isStudent = true;
+
+        System.out.println(number);
+        System.out.println(price);
+        System.out.println(grade);
+        System.out.println(isStudent);
+
+        //String array object
+
+        String name = "FrontendBoi";
+        System.out.println(name);
+        System.out.println("Hello " + name);
+
+
+
+        String learningFrom = "Bro code";
+        int gta = 6;
+        double pi = 3.14159;
+        char gender = 'M';
+        boolean isAdmin = true;
+
+        System.out.println(learningFrom);
+        System.out.println(gta);
+        System.out.println(pi);
+        System.out.println(gender);
+        System.out.println(isAdmin);
+    }
+}
