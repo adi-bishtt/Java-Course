e3e51fd692a0b3b3d883dbf02fea5e0e89b431e8
Aditya Bisht <adpycloud@gmail.com>
2026-01-07 15:41:31 +0530

    Day2 (Session 2)

---
+++ Java/src/Arithmetic.java
@@ -0,0 +1,51 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+
+        //ARITHMETIC OPERATORS
+
+//        int x = 15;
+//        int y = 2;
+//        int z;
+//
+//        //z = x + y;
+//        //z = x - y;
+//        //z = x*y;
+//        //z = x/y;
+//
+//        //z = x % y;
+//
+//
+//        System.out.println(z);
+
+
+        //Augmented Assignment Operators
+
+//        int x = 10;
+//        int y = 3;
+//
+//        x+=y;//Also works for - , * , / , %
+//
+//        System.out.println(x);
+
+        //Increment and Decrement
+
+//        int x = 1;
+//
+//        x++; //same as x = x+1;
+//        x--; //same as x = x-1;
+
+
+
+        //ORDER OF ARITHMETIC OPERATIONS
+
+        //PEMDAS(Percentage , Exponent , Multiplication , Division , Addition , Subtraction)
+
+
+
+
+
+
+
+        
+    }
+}
