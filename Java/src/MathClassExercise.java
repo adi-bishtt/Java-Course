ef9dd3ae4f7fa52c76bf5b4fb2774032f9c05add
Aditya Bisht <adpycloud@gmail.com>
2026-01-09 11:18:33 +0530

    day 4 (Session 1)

---
+++ Java/src/MathClassExercise.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class MathClassExercise {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        //Hypotenuse c of a right angle triangle
+
+
+        double side1;
+        double side2;
+        double side3;
+
+        System.out.print("Enter the Perpendicular : ");
+        side1 = scanner.nextDouble();
+
+        System.out.print("Enter the Base : ");
+        side2 = scanner.nextDouble();
+
+        double perpendicular = Math.pow(side1, 2);
+        double base = Math.pow(side2, 2);
+
+        side3 = Math.sqrt(perpendicular + base);
+        System.out.println("Hypotenuse is " + side3);
+
+        scanner.close();
+
+    }
+}
---
+++ Java/src/Mathclass.java
@@ -0,0 +1,20 @@
+public class Mathclass {
+    public static void main(String[] args) {
+
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+        double result;
+
+        result = Math.pow(3 , 3);
+        result = Math.abs(-99);
+        result = Math.sqrt(36);
+        result = Math.round(3.14);
+        result = Math.floor(3.14);
+        result = Math.ceil(3.14);
+        result = Math.max(10 , 20);
+        result = Math.min(10 , 20);
+        System.out.println(result);
+
+    }
+}
