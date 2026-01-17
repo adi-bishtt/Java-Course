253f7ef1e6d100169835d584ce41f50f492a0c98
Aditya Bisht <adpycloud@gmail.com>
2026-01-17 10:51:58 +0530

    Day 11(Session 1)

---
+++ Java/src/variableClass.java
@@ -0,0 +1,18 @@
+public class variableClass{
+
+static int x = 3;
+    public static void main(String[] args) {
+
+        // Variable scope = where a variable can be accessed...
+
+        //LOCAL VARIABLE SCOPE
+
+
+        System.out.println(x);
+
+        doSomething();
+    }
+    static void doSomething(){
+
+    }
+}
